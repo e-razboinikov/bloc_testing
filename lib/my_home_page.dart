3ccc2556e993ceeb136af6bfbb7128966362bcdd
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-01-11 11:19:13 +0500

    Connected BLoC to UI.

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './color_bloc.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -8,23 +10,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _bloc = ColorBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BLoC with Streams'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: AnimatedContainer(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            color: Colors.red,
-          ),
-          duration: const Duration(milliseconds: 500),
-          width: 100.0,
-          height: 100.0,
-        ),
-      ),
+      body: StreamBuilder<Color>(
+          stream: _bloc.outputStateStream,
+          initialData: Colors.red,
+          builder: (context, snapshot) {
+            return Center(
+              child: AnimatedContainer(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: snapshot.data,
+                ),
+                duration: const Duration(milliseconds: 500),
+                width: 100.0,
+                height: 100.0,
+              ),
+            );
+          }),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
@@ -32,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
             ),
-            onPressed: () {},
+            onPressed: () => _bloc.inputEventSink.add(ColorEvent.eventRed),
             backgroundColor: Colors.red,
           ),
           const SizedBox(
@@ -42,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
             ),
-            onPressed: () {},
+            onPressed: () => _bloc.inputEventSink.add(ColorEvent.eventGreen),
             backgroundColor: Colors.green,
           ),
         ],
