ee5587e2a278cb23e0b6bb21a18f9b9e88aef41b
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-01-16 16:35:38 +0500

    Added BlocProvider to the whole app.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,31 +1,39 @@
+import 'package:bloc_test/services/user_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../widgets/action_buttons.dart';
 import '../widgets/user_list.dart';
+import '../bloc/user_bloc.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final usersRepository = UsersRepository();
+
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Users list'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 16.0,
+    return BlocProvider<UserBloc>(
+      create: (ctx) => UserBloc(usersRepository: usersRepository),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Users list'),
+          centerTitle: true,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            ActionButtons(),
-            Expanded(
-              child: UserList(),
-            )
-          ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              ActionButtons(),
+              Expanded(
+                child: UserList(),
+              )
+            ],
+          ),
         ),
       ),
     );
