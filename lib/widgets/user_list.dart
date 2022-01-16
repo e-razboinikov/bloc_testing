50918931a363d34fe9e22527fc106b89e94d104e
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-01-16 15:23:32 +0500

    Finished working with app layout.

--- lib/widgets/user_list.dart
+++ lib/widgets/user_list.dart
@@ -5,6 +5,47 @@ class UserList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (ctx, index) => Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Text(
+            'ID: 1',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Column(
+            children: [
+              const Text(
+                'My name',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Email: test@test.com',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  Text(
+                    'Phone: 123456789',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
