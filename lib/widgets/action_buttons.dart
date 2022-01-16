3440fa220e38ff01c660d7989829f3c88427947c
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-01-16 16:38:44 +0500

    Added logic for buttons.

--- lib/widgets/action_buttons.dart
+++ lib/widgets/action_buttons.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/user_bloc.dart';
+import '../bloc/user_event.dart';
 
 class ActionButtons extends StatelessWidget {
   const ActionButtons({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final UserBloc userBloc = BlocProvider.of(context);
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            userBloc.add(
+              UserLoadEvent(),
+            );
+          },
           child: const Text(
             'Load',
           ),
@@ -21,7 +30,11 @@ class ActionButtons extends StatelessWidget {
           width: 8.0,
         ),
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            userBloc.add(
+              UserClearEvent(),
+            );
+          },
           child: const Text(
             'Clear',
           ),
